0137569c81d21e233619f1eda24085238ce2f727
Sahil Shah <sahilshah102103@gmail.com>
2022-09-25 12:10:39 -0400

    Initial Commit

---
+++ Swfit Workshop/Swfit_WorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swfit_WorkshopApp.swift
+//  Swfit Workshop
+//
+//  Created by Sahil Shah on 9/25/22.
+//
+
+import SwiftUI
+
+@main
+struct Swfit_WorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
